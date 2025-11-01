b97f50094d0a94fcf4abe464a493be461ebc49fd
Rupan Prasai <rupanprasai18@gmail.com>
2025-10-31 22:31:48 -0500

    update

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// dummy endpoint
+app.get('/version', (req, res) => {
+  res.send('1') // change this value to confirm new deployment
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
